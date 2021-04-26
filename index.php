ddae120e1256fa4fe1dde7ab14ae50bd689dc162
Mateusz Kowalski <mkmotywacja@gmail.com>
2021-04-26 14:22:48 +0200

    feat:Error handling

--- index.php
+++ index.php
@@ -4,13 +4,17 @@ declare(strict_types=1);
 
 namespace App;
 
+
 require_once("src/Utils/debug.php");
 require_once("src/Controller.php");
+
+$configuration = require_once("config/config.php");
 
 $request = [
   'get' => $_GET,
   'post' => $_POST
 ];
 
+Controller::initConfiguration($configuration);
 (new Controller($request))->run();
 
--- src/Controller.php
+++ src/Controller.php
@@ -3,18 +3,29 @@ declare(strict_types=1);
 
 namespace App;
 
-require_once("src/View.php");
+require_once("Database.php");
+require_once("View.php");
 
 class Controller{
 
     private const  DEFAULT_ACTION = 'list';
 
+    private static array $configuration = [];
+
     private array $request;
     private View $view; // Można typować również po nazwie klasy
 
+    public static function initConfiguration(array $configuration):void{
+        self::$configuration = $configuration;
+    }
+
     public function __construct(array $request){
+
+        $db = new Database(self::$configuration['db']);
+
         $this->request = $request;
         $this->view = new View();
+
     }
 
     public function run():void{
